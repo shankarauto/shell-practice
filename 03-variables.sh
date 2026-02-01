5af3ba0d2589540a722afbecce6b7f3dca3dc0ae
Ravi <ravi.k@codonsystems.com>
2026-02-01 18:28:42 -0500

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-PERSON1=Suresh
-PERSON2=Ramesh
+# PERSON1=Suresh
+# PERSON2=Ramesh
+
+# echo "$PERSON1:: Hello $PERSON2, How are you?"
+# echo "$PERSON2:: Hi $PERSON1, I am fine, thanks for asking. How are you doing"
+# echo "$PERSON1:: doing fine. what are you learning $PERSON2?"
+# echo "$PERSON2:: I am doing Shell. What about you?"
+
+
+
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine, thanks for asking. How are you doing"
