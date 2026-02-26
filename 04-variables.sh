89e763087cd850aa0137825b1e3280357bbaad78
Ravi <ravi.k@codonsystems.com>
2026-02-26 07:08:34 -0500

    actions

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Suresh
-PERSON2=Ramesh
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine, thanks for asking. How are you doing"
