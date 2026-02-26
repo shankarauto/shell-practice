5a11fc5a9d06944b9da09c51caebe8048212f5dd
Ravi <ravi.k@codonsystems.com>
2026-02-26 07:24:26 -0500

    actions

---
+++ 06-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+START_TIME = $(date + %s)
+
+echo "Script executed at ::" $START_TIME
+
+sleep 10
+
+END_TIME = $(date + %s)
+
+TOTAL_TIME = $($END_TIME - $START_TIME)
+
+echo "Script executed in : "$TOTAL_TIME" in seconds"
