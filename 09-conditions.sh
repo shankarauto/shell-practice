52d9f7544ac84eda033cb81a9a0717ba9874cf4d
Ravi <ravi.k@codonsystems.com>
2026-02-26 08:06:07 -0500

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt -> greater than
+# -lt -> lesser than
+# -eq -> equal
+# -ne -> not equal
+
+if [$NUMBER -gt 20]; then
+    echo "Given number :" $NUMBER "is greater than 20"
+fi
+
+
