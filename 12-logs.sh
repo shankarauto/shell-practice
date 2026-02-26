15f6e4b868f9d508a09e647c2ef276c9b2e19a63
Ravi <ravi.k@codonsystems.com>
2026-02-26 11:49:53 -0500

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="/var/log/shell-script/$0.log"
+LOGS_FILE="/var/log/shell-script/"$0".log"
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
     exit 1
