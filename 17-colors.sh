9349e0bfffeace0085dc3d7fb7c10bbf89213ab0
Ravi <ravi.k@codonsystems.com>
2026-02-26 13:33:10 -0500

    actions

---
+++ 17-colors.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/"$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e $R"Please run this script with root user access"$N | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e $2"..."$R "FAILURE"$N| tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e $2"..."$G "SUCCESS"$N| tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh ngins mysql nodejs
+do  
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo $package not installed, installing now
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE $? $package installation
+    else
+        echo $package already installed, skipping..
+    fi
+done
+
+# dnf install nginx -y &>> $LOGS_FILE
+# VALIDATE $? "Installing nginx"
+
+# dnf install mysql -y &>> $LOGS_FILE
+# VALIDATE $? "Installing sql"
+
+# dnf install nodejs -y &>> $LOGS_FILE
+# VALIDATE $? "Installing nodejs"
